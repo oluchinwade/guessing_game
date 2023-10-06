0b58b61b6b8c0a79cedcb89752c21fabb5cfd07d
Oluchukwu Nwade <oluchinwade@gmail.com>
2023-10-07 00:17:15 +0100

    Added hints to the guessing game

--- game.py
+++ game.py
@@ -5,20 +5,23 @@ def game():
     tries = 0
     max_tries =3
     while tries < max_tries:
-        user = input("Guess the number:    ")
+        user = input("Guess the number:   ")
         user = int(user)
         tries +=1
         if user == numb:
             print('You guessed the right number, good job!')
             break
         elif user != numb:
-        
             print(f'You still have {max_tries - tries} chances left')
-            if tries == 3:
-                print(f'Oh no you almost got it, the correct number was {numb}.')
-                break
-        else:
-            break
+            if user > numb:
+                print('Hint, the number is less than this')
+            elif user < numb:
+                print('Hint, the number is more than this')            
+        if tries == 3:
+            print(f'Oh no you almost got it, the correct number was {numb}.')
+            continue
+            
+    
             
 
 
