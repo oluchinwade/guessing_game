89c2bdb42eac83a806dd818321c7055c11997c7e
Oluchukwu Nwade <oluchinwade@gmail.com>
2023-10-06 23:18:54 +0100

    Guessing game

---
+++ game.py
@@ -0,0 +1,27 @@
+import random
+def game():
+    numb = random.randint(1,10)
+    
+    tries = 0
+    max_tries =3
+    while tries < max_tries:
+        user = input("Guess the number:    ")
+        user = int(user)
+        tries +=1
+        if user == numb:
+            print('You guessed the right number, good job!')
+            break
+        elif user != numb:
+        
+            print(f'You still have {max_tries - tries} chances left')
+            if tries == 3:
+                print(f'Oh no you almost got it, the correct number was {numb}.')
+                break
+        else:
+            break
+            
+
+
+game()
+
+        
